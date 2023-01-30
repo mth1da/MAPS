23bd51234b0b67294a3ba50ab0d1bdc58042ddd4
mth1da <113670955+mth1da@users.noreply.github.com>
2023-01-30 14:32:44 +0100

    attempt of a cool feed

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PublicationRepository;
 
 class FeedController extends AbstractController
 {
     #[Route('/feed', name: 'app_feed')]
-    public function index(): Response
+    public function index(PublicationRepository $publicationRepository): Response
     {
         return $this->render('feed/index.html.twig', [
-            'controller_name' => 'FeedController',
+            'publications' => $publicationRepository->findAll(),
         ]);
     }
 }

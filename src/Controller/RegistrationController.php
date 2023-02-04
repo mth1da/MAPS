629ec34fa83cfe7e82052f3814822c92cd50e14c
Pauline <pauline.auda@free.fr>
2023-02-04 18:16:44 +0100

    improvements

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,27 +2,18 @@
 
 namespace App\Controller;
 
-use App\Services\RegistrationFormHandler;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
-use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-
-    private UserPasswordHasherInterface $passwordHasher;
-    private EntityManagerInterface $entityManager;
     private RegistrationFormType $registrationForm;
 
-    public function __construct(UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager, RegistrationFormType $registrationForm)
+    public function __construct(RegistrationFormType $registrationForm)
     {
-        $this->passwordHasher = $passwordHasher;
-        $this->entityManager = $entityManager;
         $this->registrationForm = $registrationForm;
     }
 

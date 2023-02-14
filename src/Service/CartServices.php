9c22a87dafaee46b95b57aa6d730338706628024
Pauline <pauline.auda@free.fr>
2023-02-14 23:26:58 +0100

    improvements

--- src/Service/CartServices.php
+++ src/Service/CartServices.php
@@ -2,52 +2,7 @@
 
 namespace App\Service;
 
-use JetBrains\PhpStorm\NoReturn;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Routing\Annotation\Route;
-
 class CartServices
 {
-    #[NoReturn] #[Route('/add/{id}', name: 'add')]
-    public function add(int $id, SessionInterface $session)
-    {
-        // On récupère le panier actuel
-        $panier = $session->get("panier", []);
 
-        if(!empty($panier[$id])){
-            $panier[$id]++;
-        }else{
-            $panier[$id] = 1;
-        }
-
-        // On sauvegarde dans la session
-        $session->set("panier", $panier);
-
-        //on redirige l'utilisateur vers le panier
-        return $this->redirectToRoute("app_cart");
-    }
-    #[NoReturn] #[Route('/remove/{id}', name: 'remove')]
-    public function remove(int $id, SessionInterface $session)
-    {
-        $panier = $session->get("panier", []);
-
-        if(!empty($panier[$id])){
-            if($panier[$id] > 1){
-                $panier[$id]--;
-            }else{
-                unset($panier[$id]);
-            }
-        }
-        $session->set("panier", $panier);
-
-        return $this->redirectToRoute("app_cart");
-    }
-
-    #[NoReturn] #[Route('/delete', name: 'delete')]
-    public function deleteAll(SessionInterface $session)
-    {
-        $session->remove("panier");
-
-        return $this->redirectToRoute("app_cart");
-    }
 }

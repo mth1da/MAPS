df04964b7d2060f2bd42f0e52acf17a17ca34b5e
amandine <amandinebremont@hotmailfr>
2023-02-15 19:50:38 +0100

    cration sandwich terminée

--- src/Controller/SandwichController.php
+++ src/Controller/SandwichController.php
@@ -14,36 +14,77 @@ class SandwichController extends AbstractController
     #[Route('/sandwich', name: 'app_sandwich')]
     public function index(SessionInterface $session, IngredientRepository $ingredientRepository): Response
     {
-        $panier = $session->get('ingredients', []);
+        $contenu= $session->get('ingredients', []);
+
         //$id = $ingredient->getId();
 
-        $dataPanier = [];
+        $dataContenuSandwich = [];
         $total = 0;
-        /* foreach ($panier as $id => $quantite){
+         foreach ($contenu as $id => $quantite){
              $ingredient = $ingredientRepository->find($id);
-             $dataPanier[] = [
+             $dataContenuSandwich[] = [
                  "ingredient" => $ingredient,
                  "quantité" => $quantite
              ];
-             // $total += $ingredient->getPrice() * $quantite;
-         }*/
+             //$total+=$ingredient->getPtrice() * $quantite;
+         }
         return $this->render('sandwich/index.html.twig', [
             'controller_name' => 'SandwichController',
             'ingredients' => $ingredientRepository->findAll(),
             "total" => $total,
-            "dataPanier" => $panier
+            "dataContenuSandwich" => $dataContenuSandwich
         ]);
     }
     #[Route('/sandwich/add/{id}', name: 'app_sandwich_add_basket')]
     public function addIngredient($id, Ingredient $ingredient, SessionInterface $session, IngredientRepository $ingredientRepository){
-        $panier = $session->get('ingredients', []);
+        $contenu = $session->get('ingredients', []);
         $id = $ingredient->getId();
-        /*foreach ($panier as $id => $quantite){
-            $product = $ingredientRepository->find($id);
-        }*/
-        $session->set('ingredients', [
-            'id' => $ingredientRepository->find($id)
-        ]);
+
+        if(!empty($contenu[$id])){
+            $contenu[$id]++;
+        }else{
+            $contenu[$id]=1;
+        }
+        $session->set('ingredients', $contenu);
+        return $this->redirectToRoute('app_sandwich');
+    }
+
+
+    // sert a soustraire 1 quantité d'un ingredient
+    #[Route('/sandwich/remove/{id}', name: 'app_sandwich_remove_basket')]
+    public function removeIngredient($id, Ingredient $ingredient, SessionInterface $session, IngredientRepository $ingredientRepository){
+        $contenu = $session->get('ingredients', []);
+        $id = $ingredient->getId();
+        if(!empty($contenu[$id])){
+            if($contenu[$id] > 1) {
+                $contenu[$id]--;
+            } else {
+                unset($contenu[$id]);
+            }
+        }
+
+        $session->set("ingredients", $contenu);
+
+        return $this->redirectToRoute('app_sandwich');
+    }
+
+    // sert a supprimer toutes les quantité d'un ingredient
+    #[Route('/sandwich/delete/{id}', name: 'app_sandwich_delete')]
+    public function deleteIngredient($id, Ingredient $ingredient, SessionInterface $session, IngredientRepository $ingredientRepository){
+        $contenu = $session->get('ingredients', []);
+        $id = $ingredient->getId();
+        if(!empty($contenu[$id])){
+            unset($contenu[$id]);
+        }
+
+        $session->set("ingredients", $contenu);
+
+        return $this->redirectToRoute('app_sandwich');
+    }
+    // sert a supprimer tous le sandwich
+    #[Route('/sandwich/delete', name: 'app_sandwich_delete_all')]
+    public function deleteAllIngredient(SessionInterface $session){
+        $session->remove("ingredients");
         return $this->redirectToRoute('app_sandwich');
     }
 }

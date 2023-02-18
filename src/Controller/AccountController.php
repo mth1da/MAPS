26484f159d5b48e4d39d85019e585816729e8251
samosaaa <samra_adl@outlook.fr>
2023-02-18 13:14:02 +0100

    EDIT ACCOUNT

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use http\Client\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,5 +13,25 @@ class AccountController extends AbstractController
     public function index(): Response
     {
         return $this->render('account/index.html.twig');
+    }
+
+
+    #[Route('/account/edit', name: 'app_account_edit')]
+    public function editProfile(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfileType::class, $user );
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('account');
+        }
+        return $this->render('account/editprofile.html.twig');
     }
 }
---
+++ src/Form/EditProfileType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('first_name',TextType::class)
+            ->add('last_name',TextType::class)
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}

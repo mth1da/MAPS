4adcdef379cd35a592781bda1855ad93e9da184a
samosaaa <samra_adl@outlook.fr>
2023-02-19 00:30:06 +0100

    change of password

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -45,6 +46,36 @@ class AccountController extends AbstractController
             'editForm' => $editForm->createView(),
         ]);
     }
+
+        #[Route('/account/edit/pass', name: 'app_account_edit_pass')]
+        public function editPass(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+        {
+
+
+            if($request->isMethod('GET')){
+
+                $user = $this->getUser();
+
+                //on crée le formulaire
+                $editPass = $this->createForm(EditProfileType::class, $user );
+
+                //on traite la requête du form
+                $editPass->handleRequest($request);
+
+                //on vérifie si les 2 mots de passe sont identiques
+                if($request->get('pass') == $request->request->get('pass2')){
+                    $user->setPassword($passwordHasher->hashPassword($user, $request->request->get('pass')));
+                    $em->flush();
+                    $this->addFlash('message', 'Mot de passe à bien été mis à jours avec succès');
+
+                    return $this->redirectToRoute('app_account');
+                }else{
+                    $this->addFlash('danger', 'les deux mots de passe ne sont pas identiques');
+                }
+            }
+            return $this->render('account/editpass.html.twig');
+
+        }
 
 
 

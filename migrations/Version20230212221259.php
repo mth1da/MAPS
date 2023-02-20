d0504f793c403e87cbd3ce25d6625c16bdc3807e
samosaaa <samra_adl@outlook.fr>
2023-02-20 18:16:32 +0100

    modif bdd

--- migrations/Version20230212221259.php
+++ migrations/Version20230212221259.php
@@ -20,8 +20,8 @@ final class Version20230212221259 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD reset_token VARCHAR(100) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
+        //$this->addSql('ALTER TABLE user ADD reset_token VARCHAR(100) DEFAULT NULL');
+        //$this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void

9bcc2925f81bb9881533d32c50556d75298c3631
mth1da <113670955+mth1da@users.noreply.github.com>
2023-02-23 00:02:32 +0100

    uniformisation des titres

--- src/Form/ResetPasswordFormType.php
+++ src/Form/ResetPasswordFormType.php
@@ -13,7 +13,7 @@ class ResetPasswordFormType extends AbstractType
     {
         $builder
             ->add('password', PasswordType::class, [
-                'label' => 'Entrez votre mot de passe',
+                'label' => 'Entrez votre nouveau mot de passe',
                 'attr' => [
                     'class' => 'form-control'
                 ]

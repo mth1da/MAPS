ebeb2e122fb342ce7373b6cdfca6145bcc02410e
mth1da <113670955+mth1da@users.noreply.github.com>
2023-04-02 17:58:44 +0200

    pain fixtures

--- src/DataFixtures/SandwichFixtures.php
+++ src/DataFixtures/SandwichFixtures.php
@@ -15,7 +15,7 @@ class SandwichFixtures extends Fixture implements DependentFixtureInterface
         $MAPS = new Sandwich();
         $MAPS->setName('Le MAPS');
         $MAPS->setIsOriginal(True);
-        $MAPS->setPrice(500);
+        $MAPS->setPrice(150);
         $MAPS->setPhoto("mapsSandwich2.jpg");
         $MAPS->addSandwichIngredient($this->getReference("burger"));
         $MAPS->addSandwichIngredient($this->getReference("cheddar"));
@@ -40,10 +40,10 @@ class SandwichFixtures extends Fixture implements DependentFixtureInterface
 
         $JB = new Sandwich();
         $JB->setName('Le Jambon-Beurre');
-        $JB->setPrice(300);
+        $JB->setPrice(200);
         $JB->setIsOriginal(True);
         $JB->setPhoto("jambonBeurreSandwich.jpg");
-        $JB->addSandwichIngredient($this->getReference("mie"));
+        $JB->addSandwichIngredient($this->getReference("baguette"));
         $JB->addSandwichIngredient($this->getReference("jambon"));
         $JB->addSandwichIngredient($this->getReference("beurre"));
         $manager->persist($JB);

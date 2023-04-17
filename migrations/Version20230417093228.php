3e3152a073970eae59efcbcd513c483cb86bffc8
mth1da <113670955+mth1da@users.noreply.github.com>
2023-04-17 16:10:42 +0200

    fix migration

--- migrations/Version20230417093228.php
+++ migrations/Version20230417093228.php
@@ -20,8 +20,8 @@ final class Version20230417093228 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F529939851147ADE');
-        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_34E8BC9C51147ADE FOREIGN KEY (order_user_id) REFERENCES user (id) ON DELETE CASCADE');
+        //$this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F529939851147ADE');
+        //$this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_34E8BC9C51147ADE FOREIGN KEY (order_user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE publication ADD updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE user ADD updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
     }
